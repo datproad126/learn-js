45f1ad6a3bc5e5676f73847f258ab04395695d72
DXskeeper <42033027+datproad126@users.noreply.github.com>
2019-05-05 21:42:34 +0700

    finished lession 2

---
+++ lession2.js
@@ -0,0 +1,7 @@
+var mycrushname = "Hà Thị Ngọc Trâm";
+var aged = 20;
+var height = "1m55";
+var hobbies = "love me";
+var talent = "deep kiss";
+var is_mygirlfriend = true;
+console.log("tên ny của tôi:" + mycrushname + ", tuổi: " + aged + ", chiều cao:" + height + ", sở thích:" + hobbies + ", tài năng:" + talent + ", có là bạn gái của tôi không? " + is_mygirlfriend);
